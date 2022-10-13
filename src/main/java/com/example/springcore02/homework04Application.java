cab07efc00b697628d62bee901859bc3b4aa61d0
Bang <rlaqudrl831@naver.com>
2022-10-13 22:50:24 +0900

    Chan

---
+++ src/main/java/com/example/springcore02/homework04Application.java
@@ -0,0 +1,13 @@
+package com.example.homework04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class homework04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(homework04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcore02/Springcore02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcore02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcore02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
